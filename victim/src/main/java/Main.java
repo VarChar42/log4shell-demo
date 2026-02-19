99544085c8e92d7f539f33d165351b606c295071
Michael Wiesinger <46874921+VarChar42@users.noreply.github.com>
2026-02-19 18:20:23 +0100

    udpate

--- victim/src/main/java/Main.java
+++ victim/src/main/java/Main.java
@@ -12,7 +12,7 @@ public class Main {
     private static final Logger logger = LogManager.getLogger(Main.class);
 
     public static void main(String[] args) throws Exception {
-        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+        HttpServer server = HttpServer.create(new InetSocketAddress("0.0.0.0", 8080), 0);
 
         server.createContext("/", (HttpExchange exchange) -> {
             URI requestURI = exchange.getRequestURI();
